47163fa12dc10587624650d3c1730ab5a702da17
Vadym Kukurika <vadym.kukurika@gmail.com>
2019-05-07 12:26:57 +0300

    Fix elementNotFound bug

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -19,22 +21,29 @@ public class GoogleSearchTest {
         driver.get("https://www.google.ru/");
         WebElement queryString = driver.findElement(By.name("q"));
         queryString.sendKeys("Java");
+
+
+
         WebElement queryButton = driver.findElement(By.name("btnK"));
         queryButton.submit();
-
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.className("r")));
     }
 
     @Test
     private void isIncludeEachResultQueryWord() {
         List<WebElement> queryResults = driver.findElements(By.className("r"));
         for (WebElement result: queryResults) {
-            String resultText = result.findElement(By.className("LC20lb")).getText();
+            //Subresults also marked as "r" class
+            if(result.getTagName().equals("h3")) {
+                continue;
+            }
+            String resultText = result.findElement(By.tagName("h3")).getText();
             assertTrue(resultText.toLowerCase().contains("java"));
         }
     }
 
     @AfterSuite
     public void closeDriver() {
-//        driver.close();
+        driver.close();
     }
 }
